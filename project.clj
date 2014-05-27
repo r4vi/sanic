ab19de0710314e7ced7010b62453c8d9812e2ead
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-05-27 23:48:24 +0100

    a little dumb game

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject sanic "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+
+  :min-lein-version "2.3.4"
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+  
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds {:sanic
+            {:source-paths ["src/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/sanic.js"
+              :optimizations :whitespace
+              :pretty-print true}}}})
---
+++ src/cljs/sanic/core.cljs
@@ -0,0 +1,72 @@
+(ns sanic.core)
+
+(enable-console-print!)
+
+(def PIXI js/PIXI)
+
+(def stage (PIXI/Stage. 0x888888))
+
+(def renderer (.autoDetectRenderer PIXI 512 256))
+
+(def game-container (PIXI/DisplayObjectContainer.))
+
+(def loader (PIXI/AssetLoader. (clj->js
+                                ["img/sanic.png"
+                                 "img/bg-far.png"
+                                 "img/bg-mid.png"])))
+
+(def app (.getElementById js/document "app"))
+
+(defn apply-gravity [sprite]
+  (let [sprite-y (.-position.y sprite)]
+    (when (< (.-position.y sprite) 150)
+      (set! (.-position.y sprite) (+ sprite-y 0.3)))))
+
+;;(.-position.y sanic)
+
+(def bg-far (PIXI/TilingSprite.
+                          (.fromImage PIXI/Texture "img/bg-far.png")
+                          512 256))
+(def bg-mid (PIXI/TilingSprite.
+             (.fromImage PIXI/Texture "img/bg-mid.png")
+             512 256))
+(def sanic (.fromImage PIXI/Sprite "img/sanic-s.png"))
+
+(defn animate []
+  (.tick js/kd)
+  (.render renderer stage)
+  (apply-gravity sanic)
+  (js/requestAnimationFrame animate))
+
+
+(defn move-left []
+  (set! (.-position.x sanic) (- (.-position.x sanic) 1)))
+
+(defn move-right []
+  (set! (.-position.x sanic) (+ (.-position.x sanic) 1)))
+
+(defn move-jump []
+  (set! (.-position.y sanic) (- (.-position.y sanic) 10)))
+
+(defn init-keys []
+  (.down js/kd.LEFT move-left)
+  (.down js/kd.RIGHT move-right)
+  (.down js/kd.SPACE move-jump)
+  )
+
+(defn on-tiles-loaded [assets]
+  (set! (.-y (.-position sanic)) 150)
+  (set! (.-x (.-position sanic)) 10)
+  (set! (.-interactive sanic) true)
+  (dorun (map #(.addChild game-container %) [bg-far bg-mid sanic]))
+  (init-keys)
+  (animate))
+
+(defn setup []
+  (.addChild stage game-container)
+  (.appendChild app (.-view renderer))
+  (set! (.-onComplete loader) on-tiles-loaded)
+  (.load loader)
+  )
+
+(.addEventListener js/document "DOMContentLoaded" setup)
